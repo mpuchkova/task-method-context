7f9b217c12c126bcea519316214f0c1b1c1dec44
mpuchkova <puchkovamm2001@mail.ru>
2022-03-27 11:34:46 +0300

    task-method-context

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -1,26 +1,21 @@
 import { EventEmitter } from './EventEmitter';
 
-/*
-1) В методе subscribe подпишитесь на событие click с помощью EventEmitter.on(eventName, callback).
-В обработчике нужно увеличивать значение поля count на 1
-2)В методе unsubscribe отпишитесь от события click с помощью EventEmitter.off(eventName, callback).
-В качестве callback нужно передавать тот же самый обработчик, который был передан при подписке.
- */
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    subscribe() {
+        //1
+        this.call = () => this.count++;
+        EventEmitter.on('click', this.call);
+    },
+    unsubscribe() {
+        //2
+        EventEmitter.off('click', this.call);
+    },
 };
 
-/*
-Сделайте так, чтобы метод first вызывал метод second со своими аргументами, но в обратном порядке:
-
-obj1.first(1, 2, 3);
-// Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
- */
 export const obj1 = {
-    first(...args) {},
-    second() {
-        // здесь ничего писать не нужно
+    first(...args) {
+        this.second(...args.reverse());
     },
+    second() {},
 };
